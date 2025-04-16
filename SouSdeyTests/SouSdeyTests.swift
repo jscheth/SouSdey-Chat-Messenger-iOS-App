8f47a50a97667b27d51476f23eecb3da28d307a7
jscheth <sovichea.chet@gmail.com>
2025-04-16 12:43:59 -0700

    Initial Commit

---
+++ SouSdeyTests/SouSdeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SouSdeyTests.swift
+//  SouSdeyTests
+//
+//  Created by Jonathan Cheth on 4/16/25.
+//
+
+import Testing
+@testable import SouSdey
+
+struct SouSdeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
