bfbf6b85e597d87d854d0fd2e4e170664a50de59
jscheth <sovichea.chet@gmail.com>
2025-04-17 19:25:02 -0700

    Add the welcome screen animate

--- SouSdey/Controllers/WelcomeViewController.swift
+++ SouSdey/Controllers/WelcomeViewController.swift
@@ -13,7 +13,19 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleChar = "🙏SouSdey"
+        for letter in titleChar {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+                
+            }
+            charIndex += 1
+        }
+        
+        
     }
 
 
