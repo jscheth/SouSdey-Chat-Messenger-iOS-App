8782e74d2233455e3d53f973f2ebdc4496b0e9c7
jscheth <sovichea.chet@gmail.com>
2025-04-22 15:37:00 -0700

    Add Table View Cell

--- SouSdey/Controllers/ChatViewController.swift
+++ SouSdey/Controllers/ChatViewController.swift
@@ -13,8 +13,16 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         title = J.appName
         navigationItem.hidesBackButton = true
     }
@@ -29,5 +37,23 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: J.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
 }
---
+++ SouSdey/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  SouSdey
+//
+//  Created by Jonathan Cheth on 4/22/25.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
